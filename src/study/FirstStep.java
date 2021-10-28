8ea9409a598406640daff2bb83bd872c8e53e1d4
daidb <davidbilalov94@mail.ru>
2021-10-28 03:23:05 +0300

    FirstStep

--- src/study/FirstStep.java
+++ src/study/FirstStep.java
@@ -3,5 +3,6 @@ package study;
 public class FirstStep {
     public static void main(String[] args) {
         System.out.println("FirstStep");
+        Print.show();
     }
 }
---
+++ src/study/Print.java
@@ -0,0 +1,7 @@
+package study;
+
+public  class Print {
+    public static void show() {
+        System.out.println("Hello World!");
+    }
+}
